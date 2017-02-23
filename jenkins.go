b4788a484ed6ce95b0bdbb70845547769bc7efe2
Chris Kokosa <Chris.Kokosa@kasasa.com>
2017-02-22 22:28:27 -0600

    adding QueueBuildJob() that queues a job even if it is currently running

--- jenkins.go
+++ jenkins.go
@@ -250,7 +250,18 @@ func (j *Jenkins) BuildJob(name string, options ...interface{}) (int64, error) {
 	if len(options) > 0 {
 		params, _ = options[0].(map[string]string)
 	}
-	return job.InvokeSimple(params)
+	return job.InvokeSimple(params, false)
+}
+
+// Queue a job.
+// First parameter job name, second parameter is optional Build parameters.
+func (j *Jenkins) QueueBuildJob(name string, options ...interface{}) (int64, error) {
+	job := Job{Jenkins: j, Raw: new(JobResponse), Base: "/job/" + name}
+	var params map[string]string
+	if len(options) > 0 {
+		params, _ = options[0].(map[string]string)
+	}
+	return job.InvokeSimple(params, true)
 }
 
 func (j *Jenkins) GetNode(name string) (*Node, error) {
