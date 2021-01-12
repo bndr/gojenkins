27e7500be6863e29aa1b5ba14c72dfc77ead2b10
hui luo <luoh@vmware.com>
2021-01-11 21:34:53 -0800

    BuildJob accept map as argument rather than a slice
    Signed-off-by: Hui Luo <luoh@vmware.com>

--- jenkins.go
+++ jenkins.go
@@ -267,12 +267,8 @@ func (j *Jenkins) DeleteJob(ctx context.Context, name string) (bool, error) {
 // Invoke a job.
 // First parameter job name, second parameter is optional Build parameters.
 // Returns queue id
-func (j *Jenkins) BuildJob(ctx context.Context, name string, options ...interface{}) (int64, error) {
+func (j *Jenkins) BuildJob(ctx context.Context, name string, params map[string]string) (int64, error) {
 	job := Job{Jenkins: j, Raw: new(JobResponse), Base: "/job/" + name}
-	var params map[string]string
-	if len(options) > 0 {
-		params, _ = options[0].(map[string]string)
-	}
 	return job.InvokeSimple(ctx, params)
 }
 
