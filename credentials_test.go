b4bae26f73b233e23f0ca75cf6eca2627c50728a
hui luo <luoh@vmware.com>
2021-09-25 11:25:56 -0700

    Check when env var been set to run integration tests.
    All tests require Jenkins instance to succeed.
    
    Signed-off-by: Hui Luo <luoh@vmware.com>

--- credentials_test.go
+++ credentials_test.go
@@ -2,14 +2,17 @@ package gojenkins
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+const integration_test string = "INTEGRATION"
+
 var (
-	cm         CredentialsManager
+	cm         *CredentialsManager
 	domain     = "_"
 	dockerID   = "dockerIDCred"
 	sshID      = "sshIdCred"
@@ -19,7 +22,9 @@ var (
 )
 
 func TestCreateUsernameCredentials(t *testing.T) {
-
+	if _, ok := os.LookupEnv(integration_test); !ok {
+		return
+	}
 	cred := UsernameCredentials{
 		ID:       usernameID,
 		Scope:    scope,
@@ -41,7 +46,9 @@ func TestCreateUsernameCredentials(t *testing.T) {
 }
 
 func TestCreateFileCredentials(t *testing.T) {
-
+	if _, ok := os.LookupEnv(integration_test); !ok {
+		return
+	}
 	cred := FileCredentials{
 		ID:          fileID,
 		Scope:       scope,
@@ -63,7 +70,9 @@ func TestCreateFileCredentials(t *testing.T) {
 }
 
 func TestCreateDockerCredentials(t *testing.T) {
-
+	if _, ok := os.LookupEnv(integration_test); !ok {
+		return
+	}
 	cred := DockerServerCredentials{
 		Scope:             scope,
 		ID:                dockerID,
@@ -89,6 +98,9 @@ func TestCreateDockerCredentials(t *testing.T) {
 }
 
 func TestCreateSSHCredentialsFullFlow(t *testing.T) {
+	if _, ok := os.LookupEnv(integration_test); !ok {
+		return
+	}
 	sshCred := SSHCredentials{
 		Scope:      scope,
 		ID:         sshID,
@@ -128,8 +140,8 @@ func TestMain(m *testing.M) {
 	jenkins := CreateJenkins(nil, "http://localhost:8080", "admin", "admin")
 	jenkins.Init(ctx)
 
-	cm = CredentialsManager{J: jenkins}
-
+	cm = &CredentialsManager{J: jenkins}
+	fmt.Printf("Debug, from TestMain\n")
 	//execute tests
 	os.Exit(m.Run())
 }
