d8d5bd769964fa40763fd8a6f8a63b798ff38603
IvenGe <47211504+IvenGe@users.noreply.github.com>
2023-07-12 21:24:50 +0000

    added users just chatgpt fix not tested yet

--- users.go
+++ users.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"encoding/json"
+	"errors"
 )
 
 const (
@@ -69,4 +71,75 @@ func (j *Jenkins) DeleteUser(ctx context.Context, userName string) error {
 // Delete deletes a Jenkins account
 func (u *User) Delete() error {
 	return u.Jenkins.DeleteUser(context.Background(), u.UserName)
+}
+
+func (j *Jenkins) GetUser(ctx context.Context, userName string) (User, error) {
+	getUserContext := "/securityRealm/user/" + userName + "/api/json"
+	response, err := j.Requester.Get(ctx, getUserContext, nil, nil)
+	if err != nil {
+		return User{}, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return User{}, errors.New(fmt.Sprintf("error retrieving user. Status is %d", response.StatusCode))
+	}
+
+	var userAPIResp struct {
+		UserName string `json:"id"`
+		FullName string `json:"fullName"`
+		Email    string `json:"email"`
+	}
+
+	err = json.NewDecoder(response.Body).Decode(&userAPIResp)
+	if err != nil {
+		return User{}, err
+	}
+
+	user := User{
+		Jenkins:  j,
+		UserName: userAPIResp.UserName,
+		FullName: userAPIResp.FullName,
+		Email:    userAPIResp.Email,
+	}
+
+	return user, nil
+}
+
+
+// GetAllUsers retrieves information about all Jenkins users
+func (j *Jenkins) GetAllUsers(ctx context.Context) ([]User, error) {
+	getAllUsersContext := "/jenkins/asynchPeople/"
+	response, err := j.Requester.Get(ctx, getAllUsersContext, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("error retrieving users. Status is %d", response.StatusCode))
+	}
+
+	var usersAPIResp []struct {
+		UserName string `json:"id"`
+		FullName string `json:"fullName"`
+		Email    string `json:"email"`
+	}
+
+	err = json.NewDecoder(response.Body).Decode(&usersAPIResp)
+	if err != nil {
+		return nil, err
+	}
+
+	users := make([]User, len(usersAPIResp))
+	for i, userAPIResp := range usersAPIResp {
+		users[i] = User{
+			Jenkins:  j,
+			UserName: userAPIResp.UserName,
+			FullName: userAPIResp.FullName,
+			Email:    userAPIResp.Email,
+		}
+	}
+
+	return users, nil
 }
